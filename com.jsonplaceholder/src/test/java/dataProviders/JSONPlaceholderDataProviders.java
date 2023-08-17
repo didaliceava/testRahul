f9999ae02517d93e6b8729d61a0ee88ff89245f3
-dida- <dida1826@hotmail.fr>
2023-08-17 09:21:53 -0500

    this is commit from x

--- com.jsonplaceholder/src/test/java/dataProviders/JSONPlaceholderDataProviders.java
+++ com.jsonplaceholder/src/test/java/dataProviders/JSONPlaceholderDataProviders.java
@@ -8,12 +8,12 @@ public class JSONPlaceholderDataProviders {
     public Object[][] userEmails() {
         Object[][] data = new Object[3][2];
 
-        data[0][0] = 8;
-        data[0][1] = "Sherwood@rosamond.me";
-        data[1][0] = 3;
-        data[1][1] = "Nathan@yesenia.net";
-        data[2][0] = 5;
-        data[2][1] = "Lucio_Hettinger@annie.ca";
+        data[0][0] = 8;    data[0][1] = "Sherwood@rosamond.me";
+
+        data[1][0] = 3;     data[1][1] = "Nathan@yesenia.net";
+
+        data[2][0] = 5;       data[2][1] = "Lucio_Hettinger@annie.ca";
+
 
         return data;
     }
